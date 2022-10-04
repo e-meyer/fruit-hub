18ea48a55bc80be2f6973ac7a48a0f5c1f4ab805
e-meyer <edmundomeyer01@gmail.com>
2022-10-04 16:09:43 -0300

    Add splash screen using flutter_native_splash - need to work on image size and top black bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,7 +8,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -15,7 +15,12 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(),
+      home: Scaffold(
+        body: Text(
+          'Initial Screen',
+          style: GoogleFonts.poppins(),
+        ),
+      ),
     );
   }
 }
