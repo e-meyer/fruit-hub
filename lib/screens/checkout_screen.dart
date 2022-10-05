d7e036fbc6a02d2c1f3ae7075da622a9e90a0bf1
e-meyer <edmundomeyer01@gmail.com>
2022-10-05 13:50:11 -0300

    Create checkout screen - need to implement popAndPushNamed

---
+++ lib/screens/checkout_screen.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:fruithub/data/user.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class CheckoutScreen extends StatelessWidget {
+  const CheckoutScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    //var user = ModalRoute.of(context)!.settings.arguments as User;
+
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SvgPicture.asset(
+              'assets/confirmed-order.svg',
+              height: 180,
+            ),
+            SizedBox(height: 40),
+            Text(
+              'Order Taken',
+              style: GoogleFonts.poppins(
+                color: Color(0xFF27214D),
+                fontSize: 32,
+              ),
+            ),
+            SizedBox(height: 10),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 50),
+              child: Text(
+                'Your order have been taken and is being attended to',
+                style: GoogleFonts.poppins(
+                  color: Color(0xFF111111),
+                  fontWeight: FontWeight.w300,
+                  fontSize: 16,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            SizedBox(height: 50),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  backgroundColor: Theme.of(context).primaryColor,
+                  elevation: 0,
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 30,
+                    vertical: 15,
+                  )),
+              onPressed: () {
+                // Navigator.pushNamed(context, '/home',
+                //     arguments: User(user.name));
+              },
+              child: Text(
+                "Continue shopping",
+                style: GoogleFonts.poppins(
+                  fontSize: 16,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
